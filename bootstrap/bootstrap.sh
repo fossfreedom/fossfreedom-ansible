a0e9653fba7620e49a980745ae578eeab14ee881
David <fossfreedom@ubuntu.com>
2020-10-02 22:30:17 +0100

    append DEB control to .bashrc

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -8,4 +8,10 @@ cd ~/tmp
 
 git clone https://github.com/fossfreedom/fossfreedom-ansible.git
 
+echo 'DEBFULLNAME="David Mohammed"' >> ~/.bashrc
+echo 'DEBEMAIL="fossfreedom@ubuntu.com"' >> ~/.bashrc
+echo 'export DEBFULLNAME DEBEMAIL' >> ~/.bashrc
+
+echo 'use wormhole to send keys and extract'
+
 exit 0
