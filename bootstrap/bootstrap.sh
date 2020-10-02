9c893ba50e55c9022708e7a3be0a0124268714a2
David Mohammed <fossfreedom@ubuntu.com>
2020-10-02 22:08:34 +0100

    correct bootstrap user name

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-sudo apt install git ansible openssh-server dialog gpg seafile-cli -y
+sudo apt install git ansible openssh-server dialog gpg wormhole -y
 
 mkdir -p ~/tmp
 
 cd ~/tmp
 
-git clone https://github.com/bashfulrobot/fossfreedom-ansible.git
+git clone https://github.com/fossfreedom/fossfreedom-ansible.git
 
 exit 0
